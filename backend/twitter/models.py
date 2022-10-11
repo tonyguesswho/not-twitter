7c9f0f36e340bf5463918d18f969e5e37a089262
Anthony <anthonyu234@gmail.com>
2022-10-11 22:15:50 +0100

    add tweet model

--- backend/twitter/models.py
+++ backend/twitter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Tweet(models.Model):
+    "Model for tweet message"
+    name = models.CharField(max_length=50)
+    message = models.TextField(blank=False, null=False, max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.message} by {self.name}"
